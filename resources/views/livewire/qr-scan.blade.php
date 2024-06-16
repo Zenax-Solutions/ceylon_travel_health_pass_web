9d26fc57f801b0304f93e2afdb64b2561d26a8de
Snrox <supunnethsara410@gmail.com>
2024-06-17 00:46:27 +0530

    Update qr-scan.blade.php

--- resources/views/livewire/qr-scan.blade.php
+++ resources/views/livewire/qr-scan.blade.php
@@ -1,37 +1,40 @@
-<div wire:ignore>
+<div>
+    <div wire:ignore>
 
-    <div class="flex items-center justify-center min-h-screen bg-gradient-to-r from-green-400 to-green-500">
-        <div
-            class="w-full max-w-md p-8 transition-all duration-500 ease-in-out transform bg-white rounded-lg shadow-lg hover:scale-105 hover:shadow-2xl">
-            <div>
+        <div class="flex items-center justify-center min-h-screen bg-gradient-to-r from-green-400 to-green-500">
+            <div
+                class="w-full max-w-md p-8 transition-all duration-500 ease-in-out transform bg-white rounded-lg shadow-lg hover:scale-105 hover:shadow-2xl">
+                <div>
 
-                <img style="width: 200px" src="{{ asset('images/logo.png') }}">
+                    <img style="width: 200px" src="{{ asset('images/logo.png') }}">
+
+                </div>
+                <h2 class="mb-4 text-3xl font-bold text-center text-gray-800">QR Ticket Scanner</h2>
+
+                <div class="p-4">
+
+                    <label for="countries"
+                        class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">Selections</label>
+                    <select wire:model = "selection"
+                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+
+                        @foreach ($selectionList as $list)
+                            <option value="{{ $list->id }}"> {{ $list->shope_name ?? $list->service_name }}</option>
+                        @endforeach
+
+                    </select>
+                </div>
+
+                <div id="qr-reader" class="overflow-hidden border-4 border-gray-300 border-dashed rounded-lg "
+                    style="width: 100%; height: auto;"></div>
+                <div id="result"
+                    class="mt-4 font-bold text-center text-gray-600 transition duration-500 ease-in-out">
+                </div>
 
             </div>
-            <h2 class="mb-4 text-3xl font-bold text-center text-gray-800">QR Ticket Scanner</h2>
-
-            <div class="p-4">
-
-                <label for="countries"
-                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">Selections</label>
-                <select wire:model = "selection"
-                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-
-                    @foreach ($selectionList as $list)
-                        <option value="{{ $list->id }}"> {{ $list->shope_name ?? $list->service_name }}</option>
-                    @endforeach
-
-                </select>
-            </div>
-
-            <div wire:ignore id="qr-reader" class="overflow-hidden border-4 border-gray-300 border-dashed rounded-lg "
-                style="width: 100%; height: auto;"></div>
-            <div id="result" class="mt-4 font-bold text-center text-gray-600 transition duration-500 ease-in-out">
-            </div>
-
         </div>
-    </div>
 
+    </div>
 
     <!-- Modal -->
     <div wire:ignore>
@@ -92,6 +95,7 @@
         </div>
     </div>
 
+
     <script>
         document.addEventListener('DOMContentLoaded', function() {
             async function onScanSuccess(decodedText, decodedResult) {
@@ -108,7 +112,7 @@
 
             let html5QrCodeScanner = new Html5QrcodeScanner(
                 "qr-reader", {
-                    fps: 30,
+                    fps: 60,
                     qrbox: {
                         width: 250,
                         height: 250
@@ -172,6 +176,7 @@
 
         }
     </script>
+
 
 
 </div>
