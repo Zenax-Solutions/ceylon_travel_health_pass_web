8780dd2793d13b9265481fae93f5eaefa1460123
Snrox <supunnethsara410@gmail.com>
2024-06-22 03:27:48 +0530

    Update welcomeEmail.blade.php

--- resources/views/emails/welcomeEmail.blade.php
+++ resources/views/emails/welcomeEmail.blade.php
@@ -67,7 +67,7 @@
 <body>
     <div class="container">
         <div class="header">
-            <img src="{{ asset('images/logo.png') }}" alt="Company Logo">
+            <img style="width: 350px" src="{{ asset('images/logo.png') }}" alt="Company Logo">
         </div>
         <div class="content">
             <h1>Welcome to Ceylon Travel & Health Pass</h1>
