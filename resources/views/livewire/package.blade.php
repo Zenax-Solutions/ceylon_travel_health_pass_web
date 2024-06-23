adc57f9357991c96739a589de20e69d632bbc27f
Snrox <supunnethsara410@gmail.com>
2024-06-24 02:10:26 +0530

    Update package.blade.php

--- resources/views/livewire/package.blade.php
+++ resources/views/livewire/package.blade.php
@@ -507,7 +507,7 @@
                                                 class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Region</label>
                                             <input type="text"
                                                 class="bg-gray-50 border border-gray-300 font-bold text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                                value="{{ str_replace('_', ' ', $auth_customer?->region_type) }}"
+                                                value="{{ str_replace('_', ' ', $auth_customer?->region_type) == 'south asian' ? 'SAARC Nations' : 'Non-SAARC Nations' }}"
                                                 readonly required>
                                         </div>
 
