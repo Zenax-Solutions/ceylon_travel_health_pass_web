2a067302f7f5b426c786771cd852963dccbad381
Snrox <supunnethsara410@gmail.com>
2024-06-25 04:43:16 +0530

    Update DestinationQrScanRecordResource.php

--- app/Filament/Resources/DestinationQrScanRecordResource.php
+++ app/Filament/Resources/DestinationQrScanRecordResource.php
@@ -10,6 +10,8 @@ use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
 use Filament\Tables\Table;
+use App\Filament\Filters\DateRangeFilter;
+use Filament\Tables\Filters\SelectFilter;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
 
@@ -23,7 +25,18 @@ class DestinationQrScanRecordResource extends Resource
     {
         return $form
             ->schema([
-                //
+                Forms\Components\TextInput::make('destination_id')
+                    ->required()
+                    ->numeric(),
+                Forms\Components\TextInput::make('ticket_id')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('status')
+                    ->required()
+                    ->maxLength(255)
+                    ->default('used'),
+                Forms\Components\DatePicker::make('date')
+                    ->required(),
             ]);
     }
 
@@ -31,10 +44,35 @@ class DestinationQrScanRecordResource extends Resource
     {
         return $table
             ->columns([
-                //
+                Tables\Columns\TextColumn::make('destination.destination')
+                    ->numeric()
+                    ->searchable()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('ticket_id')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('status')
+                    ->badge()
+                    ->color(fn (string $state): string => match ($state) {
+                        'used' => 'danger',
+                    }),
+                Tables\Columns\TextColumn::make('date')
+                    ->date()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
             ])
             ->filters([
-                //
+                DateRangeFilter::make('date'),
+                SelectFilter::make('destination_id')
+                    ->relationship('destination', 'destination')
+                    ->indicator('Destination')
+                    ->label('Destination List'),
             ])
             ->actions([
                 // Tables\Actions\EditAction::make(),
