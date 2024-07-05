4a903a47b1cd587783e064eab16483a7a1639441
Supun Nethsara <supunnethsara410@gmail.com>
2024-07-04 22:07:05 -0700

    Update chatwidget.blade.php

--- resources/views/pages/home/components/chatwidget.blade.php
+++ resources/views/pages/home/components/chatwidget.blade.php
@@ -1,5 +1,5 @@
 <div class="fixed z-50 bottom-4 right-4 xl:right-20">
-    <a href="#" target="_blank"
+    <a href="https://wa.link/acspcl" target="_blank"
         class="flex items-center justify-between gap-4 px-4 py-3 font-mono font-semibold text-white duration-500 ease-in-out transform bg-green-400 rounded-lg shadow animate-bounce hover:shadow-xl">
         <img width="48" height="48" src="https://img.icons8.com/color/48/whatsapp--v1.png" alt="whatsapp--v1" />
         Live Chat
