c58c12639c05ed8067c4e2e83b42c329fe516cb7
Supun Nethsara <supunnethsara410@gmail.com>
2024-07-08 01:22:38 -0700

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,6 +7,7 @@ use App\Models\Scopes\Searchable;
 use App\Models\Traits\FilamentTrait;
 use Illuminate\Notifications\Notifiable;
 use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
@@ -40,5 +41,10 @@ class User extends Authenticatable
     public function isSuperAdmin(): bool
     {
         return in_array($this->email, config('auth.super_admins'));
+    }
+
+    public function canAccessPanel(Panel $panel): bool
+    {
+        return str_ends_with($this->email, '') && $this->hasVerifiedEmail();
     }
 }
