7ddf2c9340ff5fec47a097e2145020ecd3b4b50d
Supun Nethsara <supunnethsara410@gmail.com>
2024-07-16 02:46:33 -0700

    Update package-info.blade.php

--- resources/views/pages/home/components/package-info.blade.php
+++ resources/views/pages/home/components/package-info.blade.php
@@ -55,9 +55,9 @@
         <!-- Open sidebar button -->
         <div>
             <div class="inline-block align-bottom">
-                <a href="#" x-on:click="open = true" class="middle none center mr-3 rounded-lg bg-green-500 py-3 px-6 font-sans text-xs font-bold text-white shadow-md shadow-green-500/20 transition-all hover:shadow-lg hover:shadow-green-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none" data-ripple-light="true">
+                <button  x-on:click="open = true" class="middle none center mr-3 rounded-lg bg-green-500 py-3 px-6 font-sans text-xs font-bold text-white shadow-md shadow-green-500/20 transition-all hover:shadow-lg hover:shadow-green-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none" data-ripple-light="true">
                     More Details
-                </a>
+                </button>
             </div>
         </div>
     </div>
