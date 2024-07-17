51b872d75d82f9288e0784689b44bf097cc2e4d1
Supun Nethsara <supunnethsara410@gmail.com>
2024-07-17 02:39:34 -0700

    Add new Loading Preloader

---
+++ resources/views/pages/home/components/preloader.blade.php
@@ -0,0 +1,30 @@
+<style>
+    /* HTML: <div class="loader"></div> */
+    .loader {
+        width: 50px;
+        padding: 8px;
+        aspect-ratio: 1;
+        border-radius: 50%;
+        background: #25b09b;
+        --_m:
+            conic-gradient(#0000 10%, #000),
+            linear-gradient(#000 0 0) content-box;
+        -webkit-mask: var(--_m);
+        mask: var(--_m);
+        -webkit-mask-composite: source-out;
+        mask-composite: subtract;
+        animation: l3 1s infinite linear;
+    }
+
+    @keyframes l3 {
+        to {
+            transform: rotate(1turn)
+        }
+    }
+</style>
+
+<div class="flex items-center justify-center min-h-screen p-5 bg-gray-100 min-w-screen"  id="preloader">
+
+    <div class="loader" ></div>
+
+</div>
