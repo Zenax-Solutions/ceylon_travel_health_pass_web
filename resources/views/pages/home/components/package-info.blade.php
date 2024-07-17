7e0307528ae64d0f70ce0cad54864dddc6b4a809
Supun Nethsara <supunnethsara410@gmail.com>
2024-07-17 02:02:22 -0700

    Update package-info.blade.php

--- resources/views/pages/home/components/package-info.blade.php
+++ resources/views/pages/home/components/package-info.blade.php
@@ -1,5 +1,5 @@
 <div>
-    <div x-data="{ open: true, searchdestination: '',searchservices: '',searchshops: '', openTab: 1  }">
+    <div x-data="{ open: false, searchdestination: '',searchservices: '',searchshops: '', openTab: 1  }">
 
         <!-- Sidebar Overlay -->
         <div x-show="open" class="fixed inset-0 z-50 overflow-hidden">
@@ -28,91 +28,91 @@
                             </div>
                         </div>
                         <div class="mt-4 px-4 h-full overflow-auto">
-                        <div x-show="openTab === 1" class="transition-all duration-300">
-                            <!-- Search Input -->
-                            <div class="mt-4 px-4">
-                                <input type="text" placeholder="Search Destination here" x-model="searchdestination" class="w-full p-2 border font-normal border-green-500 rounded-md focus:border-green-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-300 transition-colors duration-300">
+                            <div x-show="openTab === 1" class="transition-all duration-300">
+                                <!-- Search Input -->
+                                <div class="mt-4 px-4">
+                                    <input type="text" placeholder="Search Destination here" x-model="searchdestination" class="w-full p-2 border font-normal border-green-500 rounded-md focus:border-green-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-300 transition-colors duration-300">
+                                </div>
+                                <div class="mt-4 px-4">
+                                    <p class="ml-2 text-gray-800 font-bold">Destination List</p>
+                                </div>
+                                <!-- Sidebar Content -->
+                                <div class="mt-4 px-4 h-full overflow-auto">
+                                    <div class="grid grid-cols-1 sm:grid-cols-2 gap-4">
+                                        @forelse($destinations as $destination)
+                                        <template x-if="searchdestination === '' || {{ json_encode($destination->destination) }}.toLowerCase().includes(searchdestination.toLowerCase())">
+                                            <div class="bg-gray-50 hover:bg-gray-100 p-4 cursor-pointer rounded-md border border-gray-300 transition-colors duration-300">
+                                                <h3 class="text-lg font-semibold text-black mb-2">{{ $destination->destination }}</h3>
+                                                <img style="object-fit: cover" class="transform border border-gray-200 rounded-md w-full h-20 object-cover" src="{{ Storage::url($destination->image) }}" />
+                                            </div>
+                                        </template>
+                                        @empty
+                                        <p>No destinations found.</p>
+                                        @endforelse
+                                    </div>
+                                </div>
+
                             </div>
-                            <div class="mt-4 px-4">
-                                <p class="ml-2 text-gray-800 font-bold">Destination List</p>
-                            </div>
-                            <!-- Sidebar Content -->
-                            <div class="mt-4 px-4 h-full overflow-auto">
-                                <div class="grid grid-cols-1 sm:grid-cols-2 gap-4">
-                                    @forelse($destinations as $destination)
-                                    <template x-if="searchdestination === '' || {{ json_encode($destination->destination) }}.toLowerCase().includes(searchdestination.toLowerCase())">
-                                        <div class="bg-gray-50 hover:bg-gray-100 p-4 cursor-pointer rounded-md border border-gray-300 transition-colors duration-300">
-                                            <h3 class="text-lg font-semibold text-black mb-2">{{ $destination->destination }}</h3>
-                                            <img style="object-fit: cover" class="transform border border-gray-200 rounded-md w-full h-20 object-cover" src="{{ Storage::url($destination->image) }}" />
-                                        </div>
-                                    </template>
-                                    @empty
-                                    <p>No destinations found.</p>
-                                    @endforelse
+
+                            <div x-show="openTab === 2" class="transition-all duration-300">
+                                <!-- Search Input -->
+                                <div class="mt-4 px-4">
+                                    <input type="text" placeholder="Search Services here" x-model="searchservices" class="w-full p-2 border font-normal border-green-500 rounded-md focus:border-green-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-300 transition-colors duration-300">
+                                </div>
+                                <div class="mt-4 px-4">
+                                    <p class="ml-2 text-gray-800 font-bold">Available services list for this package</p>
+                                </div>
+                                <!-- Sidebar Content -->
+                                <div class="mt-4 px-4 h-full overflow-auto">
+                                    <div class="grid grid-cols-1 sm:grid-cols-2 gap-4">
+                                        @if (!empty($package->discount_service_list))
+                                        @forelse($package->discount_service_list as $serviceId)
+                                        @php
+                                        $service = \App\Models\DiscountService::find($serviceId);
+                                        @endphp
+                                        <template x-if="searchservices === '' || {{ json_encode($service?->service_name) }}.toLowerCase().includes(searchservices.toLowerCase())">
+                                            <div class="bg-gray-50 hover:bg-gray-100 p-4 cursor-pointer rounded-md border border-gray-300 transition-colors duration-300">
+                                                <h3 class="text-lg font-semibold text-black mb-2">{{ $service?->service_name }}</h3>
+                                                <img style="object-fit: cover" class="transform border border-gray-200 rounded-md w-full h-20 object-cover" src="{{ Storage::url($service?->image) }}" />
+                                            </div>
+                                        </template>
+                                        @empty
+                                        <h3 class="my-2 px-4 text-[15px] text-red-400">empty list 😢</h3>
+                                        @endforelse
+                                        @endif
+                                    </div>
                                 </div>
                             </div>
 
-                        </div>
-
-                        <div x-show="openTab === 2" class="transition-all duration-300">
-                            <!-- Search Input -->
-                            <div class="mt-4 px-4">
-                                <input type="text" placeholder="Search Destination here" x-model="searchservices" class="w-full p-2 border font-normal border-green-500 rounded-md focus:border-green-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-300 transition-colors duration-300">
-                            </div>
-                            <div class="mt-4 px-4">
-                                <p class="ml-2 text-gray-800 font-bold">Available services list for this package</p>
-                            </div>
-                            <!-- Sidebar Content -->
-                            <div class="mt-4 px-4 h-full overflow-auto">
-                                <div class="grid grid-cols-1 sm:grid-cols-2 gap-4">
-                                    @if (!empty($package->discount_service_list))
-                                    @forelse($package->discount_service_list as $serviceId)
-                                    @php
-                                    $service = \App\Models\DiscountService::find($serviceId);
-                                    @endphp
-                                    <template x-if="searchservices === '' || {{ json_encode($service?->service_name) }}.toLowerCase().includes(searchservices.toLowerCase())">
-                                        <div class="bg-gray-50 hover:bg-gray-100 p-4 cursor-pointer rounded-md border border-gray-300 transition-colors duration-300">
-                                            <h3 class="text-lg font-semibold text-black mb-2">{{ $service?->service_name }}</h3>
-                                            <img style="object-fit: cover" class="transform border border-gray-200 rounded-md w-full h-20 object-cover" src="{{ Storage::url($service?->image) }}" />
-                                        </div>
-                                    </template>
-                                    @empty
-                                    <h3 class="my-2 px-4 text-[15px] text-red-400">empty list 😢</h3>
-                                    @endforelse
-                                    @endif
+                            <div x-show="openTab === 3" class="transition-all duration-300">
+                                <!-- Search Input -->
+                                <div class="mt-4 px-4">
+                                    <input type="text" placeholder="Search Shops here" x-model="searchshops" class="w-full p-2 border font-normal border-green-500 rounded-md focus:border-green-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-300 transition-colors duration-300">
+                                </div>
+                                <div class="mt-4 px-4">
+                                    <p class="ml-2 text-gray-800 font-bold">Available shops list for this package</p>
+                                </div>
+                                <!-- Sidebar Content -->
+                                <div class="mt-4 px-4 h-full overflow-auto">
+                                    <div class="grid grid-cols-1 sm:grid-cols-2 gap-4">
+                                        @if (!empty($package->discount_shop_list))
+                                        @forelse($package->discount_shop_list as $shopId)
+                                        @php
+                                        $shop = \App\Models\DiscountShop::find($shopId);
+                                        @endphp
+                                        <template x-if="searchshops === '' || {{ json_encode($shop?->shope_name) }}.toLowerCase().includes(searchshops.toLowerCase())">
+                                            <div class="bg-gray-50 hover:bg-gray-100 p-4 cursor-pointer rounded-md border border-gray-300 transition-colors duration-300">
+                                                <h3 class="text-lg font-semibold text-black mb-2">{{ $shop?->shope_name }}</h3>
+                                                <img style="object-fit: cover" class="transform border border-gray-200 rounded-md w-full h-20 object-cover" src="{{ Storage::url($shop?->image) }}" />
+                                            </div>
+                                        </template>
+                                        @empty
+                                        <h3 class="my-2 px-4 text-[15px] text-red-400">empty list 😢</h3>
+                                        @endforelse
+                                        @endif
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-
-                        <div x-show="openTab === 3" class="transition-all duration-300">
-                            <!-- Search Input -->
-                            <div class="mt-4 px-4">
-                                <input type="text" placeholder="Search Destination here" x-model="searchshops" class="w-full p-2 border font-normal border-green-500 rounded-md focus:border-green-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-300 transition-colors duration-300">
-                            </div>
-                            <div class="mt-4 px-4">
-                                <p class="ml-2 text-gray-800 font-bold">Available shops list for this package</p>
-                            </div>
-                            <!-- Sidebar Content -->
-                            <div class="mt-4 px-4 h-full overflow-auto">
-                                <div class="grid grid-cols-1 sm:grid-cols-2 gap-4">
-                                    @if (!empty($package->discount_shop_list))
-                                    @forelse($package->discount_shop_list as $shopId)
-                                    @php
-                                    $shop = \App\Models\DiscountShop::find($shopId);
-                                    @endphp
-                                    <template x-if="searchshops === '' || {{ json_encode($shop?->shope_name) }}.toLowerCase().includes(searchshops.toLowerCase())">
-                                        <div class="bg-gray-50 hover:bg-gray-100 p-4 cursor-pointer rounded-md border border-gray-300 transition-colors duration-300">
-                                            <h3 class="text-lg font-semibold text-black mb-2">{{ $shop?->shope_name }}</h3>
-                                            <img style="object-fit: cover" class="transform border border-gray-200 rounded-md w-full h-20 object-cover" src="{{ Storage::url($shop?->image) }}" />
-                                        </div>
-                                    </template>
-                                    @empty
-                                    <h3 class="my-2 px-4 text-[15px] text-red-400">empty list 😢</h3>
-                                    @endforelse
-                                    @endif
-                                </div>
-                            </div>
-                        </div>
 
 
                         </div>
