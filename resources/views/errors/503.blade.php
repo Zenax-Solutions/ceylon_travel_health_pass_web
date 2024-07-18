cb09b8217a4026e07a94be7a1fc79b6766007785
Supun Nethsara <supunnethsara410@gmail.com>
2024-07-18 12:30:11 -0700

    Improve Maintence page

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -7,10 +7,11 @@
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body>
-<div class="bg-gray-100">
+<div class="bg-green-400">
   <div class="min-h-screen flex flex-col justify-center items-center">
+    <x-logo/>
     <img src="https://www.svgrepo.com/show/426192/cogs-settings.svg" alt="Logo" class="mb-8 h-40">
-    <h1 class="text-4xl md:text-5xl lg:text-6xl font-bold text-center text-gray-700 mb-4">Site is under maintenance</h1>
+    <h1 class="text-4xl md:text-5xl lg:text-6xl font-bold text-center text-white mb-4">Site is under maintenance</h1>
     <p class="text-center text-gray-500 text-lg md:text-xl lg:text-2xl mb-8">We're working hard to improve the user experience. Stay tuned!</p>
   </div>
 </div>
