d81979b3a6ef1a9b88e733c347c7cd05e033b87d
Supun Nethsara <supunnethsara410@gmail.com>
2024-07-21 09:13:09 -0700

    Update home.blade.php

--- resources/views/mobile/home.blade.php
+++ resources/views/mobile/home.blade.php
@@ -66,7 +66,7 @@
                 </div>
                 <div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
                     <div class="sm:mx-auto sm:w-full sm:max-w-sm pb-2">
-                        <img class="mx-auto w-auto" style="width: 60%;" src="{{asset('images/icons/agent.png')}}">
+                        <img class="mx-auto w-auto" style="width: 45%;" src="{{asset('images/icons/agent.png')}}">
                        
                         <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight to-blue-500">Agent Portal</h2>
                     </div>
@@ -76,7 +76,7 @@
                                Log-In
                             </a>
                     <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm pb-2">
-                        <img class="mx-auto " style="width: 60%;" src="{{asset('images/icons/destination.png')}}" >
+                        <img class="mx-auto " style="width: 45%;" src="{{asset('images/icons/destination.png')}}" >
                         <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight to-blue-500">Destination Portal</h2>
                     </div>
                     <a href="{{ route('destination.login') }}"
