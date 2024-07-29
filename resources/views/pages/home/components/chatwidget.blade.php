51f6aac0c0b53f46ca555b4d83fb30d9a98128e1
Snrox <supunnethsara410@gmail.com>
2024-07-30 00:05:28 +0530

    Update chatwidget.blade.php

--- resources/views/pages/home/components/chatwidget.blade.php
+++ resources/views/pages/home/components/chatwidget.blade.php
@@ -2,6 +2,6 @@
     <a href="https://wa.link/acspcl" target="_blank"
         class="flex items-center justify-between gap-4 px-4 py-3 font-mono font-semibold text-white duration-500 ease-in-out transform bg-black rounded-lg shadow animate-bounce hover:shadow-xl">
         <img width="48" height="48" src="https://img.icons8.com/color/48/speech-bubble-with-dots.png"
-            alt="speech-bubble-with-dots" /> live chat
+            alt="speech-bubble-with-dots" />
     </a>
 </div>
