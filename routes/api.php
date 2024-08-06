aaba8e7042a3beb52f67e4edb803eba9672f05ac
Supun Nethsara <supunnethsara410@gmail.com>
2024-08-06 10:49:47 -0700

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -35,10 +35,10 @@ use App\Http\Controllers\PaymentController;
 
 Route::post('/login', [AuthController::class, 'login'])->name('api.login');
 
-Route::get('/payment/process', [PaymentController::class, 'process'])->name('payment.process');
-Route::get('/payment/return', [PaymentController::class, 'handleReturn'])->name('payment.return');
-Route::get('/payment/cancel', [PaymentController::class, 'handleCancel'])->name('payment.cancel');
-Route::post('/payment/notify', [PaymentController::class, 'handleNotify'])->name('payment.notify');
+Route::get('/api/payment/process', [PaymentController::class, 'process'])->name('payment.process');
+Route::get('/api/payment/return', [PaymentController::class, 'handleReturn'])->name('payment.return');
+Route::get('/api/payment/cancel', [PaymentController::class, 'handleCancel'])->name('payment.cancel');
+Route::post('/api/payment/notify', [PaymentController::class, 'handleNotify'])->name('payment.notify');
 
 Route::middleware('auth:sanctum')
     ->get('/user', function (Request $request) {
